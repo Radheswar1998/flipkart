20a2759b9148e6f1394c629353a755c09ee3137e
radheswar <20906447+Radheswar1998@users.noreply.github.com>
2019-07-03 00:26:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+var client = require('flipkart-api-affiliate-client');
+var fkClient = new client({
+    trackingId:"yuganshch",
+    token:"34adf1305516462d8ce93de45594da81",
+},"json");
+app.get('/',function(req,res){
+	res.sendFile(__dirname +'/public/index.html')
+})
+app.get('/search',function(req,res){
+	fkClient.doKeywordSearch(req.query.key,1).then(function(value){
+        console.log(value); //object with status, error and body
+        res.send(value.body)
+    });
+});
+app.listen(5000,function(){
+	console.log("server started")
+})
